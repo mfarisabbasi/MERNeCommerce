44be65d41542c95a089e8bed0fbaca3deb6d5f5f
Faris Abbasi <mfarisabbasi@gmail.com>
2020-12-12 00:22:27 -0800

    added product search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ const App = () => {
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
           <Route path="/admin/productlist" component={ProductListScreen} />
           <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
+          <Route path="/search/:keyword" component={HomeScreen} />
           <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
